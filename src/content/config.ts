7c93c9fe2f494e48257cb111065a42570f86c722
cryptobox <pray.happy76@gmail.com>
2024-09-14 10:49:40 +0900

    feat: 一覧を仮で作成

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,8 @@ const blog = defineCollection({
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
+    // TODO: 後でoptional消す
+    tags: z.array(z.string()),
   }),
 });
 
