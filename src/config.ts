dfa26deb526728f5ed397a4c33a305f373d169cc
cryptobox <pray.happy76@gmail.com>
2024-09-21 19:54:17 +0900

    feat: 外部リンクを実装

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+export const profileConfig = {
+  links: [
+    {
+      name: "X",
+      icon: "x-logo",
+      to: "https://x.com/cryptooooon",
+    },
+    {
+      name: "Zenn",
+      icon: "zenn-logo",
+      to: "https://zenn.dev/cryptobox",
+    },
+    {
+      name: "Github",
+      icon: "github-logo",
+      to: "https://github.com/crypt0box",
+    },
+  ],
+} as const;
