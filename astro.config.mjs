c38965c3c226879e52a9c08f17e1c58c837faaf0
cryptobox <pray.happy76@gmail.com>
2024-10-26 09:25:24 +0900

    fix: URLを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), react(), tailwind(), icon()],
+  site: "https://cryptobox.blog/",
+  integrations: [mdx(), sitemap(), react(), tailwind(), icon()],
 });
