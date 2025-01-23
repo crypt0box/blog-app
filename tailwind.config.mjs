3a87dc085449019ac580fc04059f10995379006b
cryptobox <pray.happy76@gmail.com>
2025-01-23 22:13:17 +0900

    Revert "fix: スタイル崩れの修正"
    This reverts commit 652ff14c3784b68f350921c58b4436ab81efacfe.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            blockquote: {
+              "word-break": "break-all",
+            },
+          },
+        },
+      },
+    },
+  },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
