e7b1226726dcfa52d4695244fa20cdf22c66947c
cryptobox <pray.happy76@gmail.com>
2025-05-11 15:20:10 +0900

    chore: tailwindをv4に更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
-
 import remarkDirective from "remark-directive";
 import remarkContainerDirectives from "./src/plugins/remarkMessage";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://cryptobox.blog",
-  integrations: [mdx(), sitemap(), react(), tailwind(), icon()],
+  integrations: [mdx(), sitemap(), react(), icon()],
   markdown: {
     remarkPlugins: [remarkDirective, remarkContainerDirectives],
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
