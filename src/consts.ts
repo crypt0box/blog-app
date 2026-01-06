fe18349a990102ad7553a944c0fba5858330e82e
cryptobox <pray.happy76@gmail.com>
2026-01-06 21:07:55 +0900

    fix: ページサイズの修正

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@ export const SITE_TITLE = "cryptobox tech blog";
 
 export const SITE_DESCRIPTION = "Welcome to my website!";
 
-export const PAGE_SIZE = 3;
+export const PAGE_SIZE = 10;
 
 export const MAX_VISIBLE = 5;
